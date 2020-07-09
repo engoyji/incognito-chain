8fa5eeae5eea1224c778ef552dc2f123680c6f6a
0xakk0r0kamui <hy@autonomous.nyc>
2020-07-09 20:03:24 +0700

    Merge branch 'testnet-optimize' of https://github.com/incognitochain/incognito-chain into testnet-optimize

--- rpcserver/rpcservice/blockservice.go
+++ rpcserver/rpcservice/blockservice.go
@@ -561,7 +561,7 @@ func (blockService BlockService) CheckHashValue(hashStr string) (isTransaction b
 			isBeaconBlock = true
 			return
 		} else {
-			_, _, _, _, err = blockService.BlockChain.GetTransactionByHash(*hash)
+			_, _, _, _, _, err = blockService.BlockChain.GetTransactionByHash(*hash)
 			if err == nil {
 				isTransaction = true
 			} else {
